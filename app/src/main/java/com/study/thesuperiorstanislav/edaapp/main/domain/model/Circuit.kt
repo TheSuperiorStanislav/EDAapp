ee3b1ed0e41f08d5889fc9686d32168f731b00e1
thssuperiorstanislav <stasontos2@hotmail.com>
2018-10-02 19:09:41 +0700

    add Circuit

---
+++ app/src/main/java/com/study/thesuperiorstanislav/edaapp/main/domain/model/Circuit.kt
@@ -0,0 +1,5 @@
+package com.study.thesuperiorstanislav.edaapp.main.domain.model
+
+class Circuit(listElements: MutableList<Element>,
+              listNets: MutableList<Net>,
+              listPins: MutableList<String>)
