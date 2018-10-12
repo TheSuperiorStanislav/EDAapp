31f7425d1c7e94558e5a2ad16788d36ea5bf4673
thssuperiorstanislav <stasontos2@hotmail.com>
2018-10-12 15:49:08 +0700

    migrate to androidX

--- app/src/main/java/com/study/thesuperiorstanislav/edaapp/main/MainFragment.kt
+++ app/src/main/java/com/study/thesuperiorstanislav/edaapp/main/MainFragment.kt
@@ -6,8 +6,8 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v4.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
