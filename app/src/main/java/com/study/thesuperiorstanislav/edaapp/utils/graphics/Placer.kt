5a1d25ad4a956fae25c58f1e1af2852e6b250ea3
thssuperiorstanislav <stasontos2@hotmail.com>
2018-11-02 14:34:17 +0700

    make code compact

--- app/src/main/java/com/study/thesuperiorstanislav/edaapp/utils/graphics/Placer.kt
+++ app/src/main/java/com/study/thesuperiorstanislav/edaapp/utils/graphics/Placer.kt
@@ -89,82 +89,98 @@ class Placer(private val drawMatrix: Array<Array<DrawObject?>>,
         element.getPins().forEachIndexed { index, pin ->
             when (index + 1) {
                 1 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * y), ObjectType.Pin, DrawType.PIN_CORNER_UP_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * y),
+                            ObjectType.Pin, DrawType.PIN_CORNER_UP_LEFT)
                     drawMatrix[y][x] = drawObject
                     pin.move(x, y)
                 }
                 2 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * y), ObjectType.Pin, DrawType.PIN_CORNER_UP_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * y),
+                            ObjectType.Pin, DrawType.PIN_CORNER_UP_RIGHT)
                     drawMatrix[y][x + 1] = drawObject
                     pin.move(x + 1, y)
                 }
                 3 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 1][x] = drawObject
                     pin.move(x, y + 1)
                 }
                 4 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 1)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 1)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 1][x + 1] = drawObject
                     pin.move(x + 1, y + 1)
                 }
                 5 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 2)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 2)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 2][x] = drawObject
                     pin.move(x, y + 2)
                 }
                 6 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 2)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 2)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 2][x + 1] = drawObject
                     pin.move(x + 1, y + 2)
                 }
                 7 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 3)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 3)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 3][x] = drawObject
                     pin.move(x, y + 3)
                 }
                 8 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 3)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 3)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 3][x + 1] = drawObject
                     pin.move(x + 1, y + 3)
                 }
                 9 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 4)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 4)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 4][x] = drawObject
                     pin.move(x, y + 4)
                 }
                 10 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 4)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 4)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 4][x + 1] = drawObject
                     pin.move(x + 1, y + 4)
                 }
                 11 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 5)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 5)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 5][x] = drawObject
                     pin.move(x, y + 5)
                 }
                 12 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 5)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 5)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 5][x + 1] = drawObject
                     pin.move(x + 1, y + 5)
                 }
                 13 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 6)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 6)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 6][x] = drawObject
                     pin.move(x, y + 6)
                 }
                 14 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 6)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 6)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 6][x + 1] = drawObject
                     pin.move(x + 1, y + 6)
                 }
                 15 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 7)), ObjectType.Pin, DrawType.PIN_CORNER_DOWN_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 7)),
+                            ObjectType.Pin, DrawType.PIN_CORNER_DOWN_LEFT)
                     drawMatrix[y + 7][x] = drawObject
                     pin.move(x, y + 7)
                 }
                 16 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 7)), ObjectType.Pin, DrawType.PIN_CORNER_DOWN_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 7)),
+                            ObjectType.Pin, DrawType.PIN_CORNER_DOWN_RIGHT)
                     drawMatrix[y + 7][x + 1] = drawObject
                     pin.move(x + 1, y + 7)
                 }
@@ -177,42 +193,50 @@ class Placer(private val drawMatrix: Array<Array<DrawObject?>>,
         element.getPins().forEachIndexed { index, pin ->
             when (index + 1) {
                 1 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * y), ObjectType.Pin, DrawType.PIN_CORNER_UP_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * y),
+                            ObjectType.Pin, DrawType.PIN_CORNER_UP_LEFT)
                     drawMatrix[y][x] = drawObject
                     pin.move(x, y)
                 }
                 2 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * y), ObjectType.Pin, DrawType.PIN_CORNER_UP_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * y),
+                            ObjectType.Pin, DrawType.PIN_CORNER_UP_RIGHT)
                     drawMatrix[y][x + 1] = drawObject
                     pin.move(x + 1, y)
                 }
                 3 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 1][x] = drawObject
                     pin.move(x, y + 1)
                 }
                 4 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 1)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 1)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 1][x + 1] = drawObject
                     pin.move(x + 1, y + 1)
                 }
                 5 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 2)), ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 2)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_LEFT)
                     drawMatrix[y + 2][x] = drawObject
                     pin.move(x, y + 2)
                 }
                 6 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 2)), ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 2)),
+                            ObjectType.Pin, DrawType.PIN_SIDE_RIGHT)
                     drawMatrix[y + 2][x + 1] = drawObject
                     pin.move(x + 1, y + 2)
                 }
                 7 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 3)), ObjectType.Pin, DrawType.PIN_CORNER_DOWN_LEFT)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 3)),
+                            ObjectType.Pin, DrawType.PIN_CORNER_DOWN_LEFT)
                     drawMatrix[y + 3][x] = drawObject
                     pin.move(x, y + 3)
                 }
                 8 -> {
-                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 3)), ObjectType.Pin, DrawType.PIN_CORNER_DOWN_RIGHT)
+                    val drawObject = DrawObject(DrawPoint(step * (x + 1), step * (y + 3)),
+                            ObjectType.Pin, DrawType.PIN_CORNER_DOWN_RIGHT)
                     drawMatrix[y + 3][x + 1] = drawObject
                     pin.move(x + 1, y + 3)
                 }
@@ -225,12 +249,14 @@ class Placer(private val drawMatrix: Array<Array<DrawObject?>>,
         element.getPins().forEachIndexed { index, pin ->
             when (index + 1) {
                 1 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * y), ObjectType.Pin, DrawType.PIN_LINE_UP)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * y),
+                            ObjectType.Pin, DrawType.PIN_LINE_UP)
                     drawMatrix[y][x] = drawObject
                     pin.move(x, y)
                 }
                 2 -> {
-                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)), ObjectType.Pin, DrawType.PIN_LINE_DOWN)
+                    val drawObject = DrawObject(DrawPoint(step * x, step * (y + 1)),
+                            ObjectType.Pin, DrawType.PIN_LINE_DOWN)
                     drawMatrix[y + 1][x] = drawObject
                     pin.move(x, y + 1)
                 }
@@ -243,7 +269,8 @@ class Placer(private val drawMatrix: Array<Array<DrawObject?>>,
         drawMatrix.forEachReversedWithIndex { yIndex, arrayOfDrawObjects ->
             arrayOfDrawObjects.forEachReversedWithIndex { xIndex, _ ->
                 if (checkNetPosition(xIndex, yIndex)) {
-                    val drawObject = DrawObject(DrawPoint(step * xIndex, step * yIndex), ObjectType.Net, DrawType.NET)
+                    val drawObject = DrawObject(DrawPoint(step * xIndex, step * yIndex),
+                            ObjectType.Net, DrawType.NET)
                     drawMatrix[yIndex][xIndex] = drawObject
                     net.move(xIndex, yIndex)
                     return
